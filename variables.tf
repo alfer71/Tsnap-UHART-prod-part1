03ddfe5d55572a2f62e52c533b8951567536b418
Alberto Fernandez <linuxxroot@gmail.com>
2025-06-25 20:55:28 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -23,7 +23,7 @@ variable "serverlist" {
     "uhart-pssba-001.hartford.edu" = "421a5980-bf4a-9905-ba3e-3be73a9c7932"
     "uhart-pwkfa-001.hartford.edu" = "421a5f13-70d8-7559-19b7-e9990bdb36c7"
     "dw-java-prod-01.hartford.edu" = "421aba01-4977-505b-eabc-4caa5409c630"
-    "dw-db-prod-01.hartford.edu" = "421af5f7-f5d5-ab52-6bee-69f866b21eaf"
+#    "dw-db-prod-01.hartford.edu" = "421af5f7-f5d5-ab52-6bee-69f866b21eaf"
     "dw-app-prod-01.hartford.edu" = "421a7619-b342-6574-6b9d-60e14785e8a1"
   }
 }
